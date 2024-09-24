b0dfb3506a12c134e13f7ec524a8b75b38bb14af
Tingfu-Zhou <782234324@qq.com>
2024-09-24 22:32:05 +0800

    9.24report

--- Prompt2.py
+++ Prompt2.py
@@ -9,7 +9,7 @@ response = client.chat.completions.create(
             "content": "You need to help me complete the sentiment analysis. You need to judge the user's emotion based on the input.\
                 hint: Are there Opinion texts in the input text? If so, is it positive or negative? Are there slang, irony, sarcasm, abbreviations,\
                 and emoticons in the text? Is there facial information in the video message? If so, is this facial information positive or negative? \
-                Is there a smile in the visual message? Is there eye contact in facial information?"
+                Is there a smile in the visual message? Is there eye contact in facial information? Keep your answers brief."
         },
         {
             "role": "user",
--- Prompt3.py
+++ Prompt3.py
@@ -3,7 +3,7 @@ client = OpenAI()
 
 assistant = client.beta.assistants.create(
   name="EMOchecker",
-  instructions="You are a personal math tutor. Write and run code to answer math questions.",
+  instructions="You are a sentiment analysis assistant and you need to analyze the user's emotions. Keep your answers brief.",
   tools=[{"type": "code_interpreter"}],
   model="gpt-4o",
 )
